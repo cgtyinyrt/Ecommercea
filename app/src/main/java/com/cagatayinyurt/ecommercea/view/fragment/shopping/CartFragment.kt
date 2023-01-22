35424c3bd817788d95776d6babe758f5b2e265c1
Çağatay İnyurt <cagatayinyurt@outlook.com>
2023-01-22 18:02:31 +0300

    CartFragment.kt, CartViewModel.kt update.

--- app/src/main/java/com/cagatayinyurt/ecommercea/view/fragment/shopping/CartFragment.kt
+++ app/src/main/java/com/cagatayinyurt/ecommercea/view/fragment/shopping/CartFragment.kt
@@ -1,7 +1,151 @@
 package com.cagatayinyurt.ecommercea.view.fragment.shopping
 
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cagatayinyurt.ecommercea.R
+import com.cagatayinyurt.ecommercea.adapter.CartProductAdapter
+import com.cagatayinyurt.ecommercea.databinding.FragmentCartBinding
+import com.cagatayinyurt.ecommercea.firebase.FirebaseCommon
+import com.cagatayinyurt.ecommercea.util.Resource
+import com.cagatayinyurt.ecommercea.util.VerticalItemDecor
+import com.cagatayinyurt.ecommercea.viewmodel.CartViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 class CartFragment: Fragment(R.layout.fragment_cart) {
+
+    private lateinit var binding : FragmentCartBinding
+    private val cartAdapter by lazy { CartProductAdapter() }
+    private val viewModel  by activityViewModels<CartViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCartBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupCartRecyclerView()
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.productPrice.collectLatest { price ->
+                price?.let {
+                    binding.tvTotalPrice.text = "$ $price"
+                }
+            }
+        }
+
+        cartAdapter.onProductClick = {
+            val bundle = Bundle().apply {
+                putParcelable("product", it.product)
+            }
+            findNavController().navigate(R.id.action_cartFragment_to_productDetailsFragment, bundle)
+        }
+
+        cartAdapter.onPlusClick = {
+            viewModel.changeQuantity(it, FirebaseCommon.QuantityChanging.INCREASE)
+        }
+
+        cartAdapter.onMinusClick = {
+            viewModel.changeQuantity(it, FirebaseCommon.QuantityChanging.DECREASE)
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.deleteDialog.collectLatest {
+                val alertDialog = AlertDialog.Builder(requireContext()).apply {
+                    setTitle("Delete Item")
+                    setMessage("Are you sure delete this item?")
+                    setNegativeButton("Cancel") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    setPositiveButton("Yes") { dialog, _ ->
+                        viewModel.deleteCartProduct(it)
+                        dialog.dismiss()
+                    }
+                }
+                alertDialog.create()
+                alertDialog.show()
+            }
+        }
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.cartProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.progressbarCart.visibility = View.VISIBLE
+                    }
+                    is Resource.Success -> {
+                        binding.progressbarCart.visibility = View.INVISIBLE
+                        if (it.data!!.isEmpty()) {
+                            showEmptyCart()
+                            hideOtherViews()
+                        } else {
+                            hideEmptyCart()
+                            showOtherViews()
+                            cartAdapter.differ.submitList(it.data)
+                        }
+                    }
+                    is Resource.Error -> {
+                        binding.progressbarCart.visibility = View.INVISIBLE
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    private fun showOtherViews() {
+        binding.apply {
+            rvCart.visibility = View.VISIBLE
+            totalBoxContainer.visibility = View.VISIBLE
+            buttonCheckout.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideOtherViews() {
+        binding.apply {
+            rvCart.visibility = View.GONE
+            totalBoxContainer.visibility = View.GONE
+            buttonCheckout.visibility = View.GONE
+        }
+    }
+
+    private fun showEmptyCart() {
+        binding.apply {
+            layoutCartEmpty.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideEmptyCart() {
+        binding.apply {
+            layoutCartEmpty.visibility = View.GONE
+        }
+    }
+
+    private fun setupCartRecyclerView() {
+        binding.rvCart.apply {
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                RecyclerView.VERTICAL,
+                false
+            )
+            adapter = cartAdapter
+            addItemDecoration(VerticalItemDecor())
+        }
+    }
 }
--- app/src/main/java/com/cagatayinyurt/ecommercea/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/cagatayinyurt/ecommercea/viewmodel/CartViewModel.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cagatayinyurt.ecommercea.data.CartProduct
 import com.cagatayinyurt.ecommercea.firebase.FirebaseCommon
+import com.cagatayinyurt.ecommercea.helper.getProductPrice
 import com.cagatayinyurt.ecommercea.util.Resource
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,7 +25,38 @@ class CartViewModel @Inject constructor(
         MutableStateFlow<Resource<List<CartProduct>>>(Resource.Unspecified())
     val cartProducts = _cartProducts.asStateFlow()
 
+    val productPrice = cartProducts.map {
+        when (it) {
+            is Resource.Success -> {
+                calculatePrice(it.data!!)
+            }
+            else -> null
+        }
+    }
+
+    private val _deleteDialog = MutableSharedFlow<CartProduct>()
+    val deleteDialog = _deleteDialog.asSharedFlow()
+
     private var cardProductDocuments = emptyList<DocumentSnapshot>()
+
+    fun deleteCartProduct(cartProduct: CartProduct) {
+        val index = cartProducts.value.data?.indexOf(cartProduct)
+        if (index != null && index != -1) {
+            val documentId = cardProductDocuments[index].id
+            firestore.collection("user")
+                .document(auth.uid!!)
+                .collection("cart")
+                .document(documentId)
+                .delete()
+        }
+    }
+
+    private fun calculatePrice(data: List<CartProduct>?): Float {
+        return data!!.sumByDouble { cartProduct ->
+            (cartProduct.product.offerPercentage.getProductPrice(cartProduct.product.price)
+                    * cartProduct.quantity).toDouble()
+        }.toFloat()
+    }
 
     init {
         getCartProducts()
@@ -54,9 +85,15 @@ class CartViewModel @Inject constructor(
             val documentId = cardProductDocuments[index].id
             when (quantityChanging) {
                 FirebaseCommon.QuantityChanging.INCREASE -> {
+                    viewModelScope.launch { _cartProducts.emit(Resource.Loading()) }
                     increaseQuantity(documentId)
                 }
                 FirebaseCommon.QuantityChanging.DECREASE -> {
+                    if (cartProduct.quantity == 1) {
+                        viewModelScope.launch { _deleteDialog.emit(cartProduct) }
+                        return
+                    }
+                    viewModelScope.launch { _cartProducts.emit(Resource.Loading()) }
                     decreaseQuantity(documentId)
                 }
             }
