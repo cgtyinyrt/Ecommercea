989906180d6d75c64713415952106b52823e0ecc
Çağatay İnyurt <cagatayinyurt@outlook.com>
2023-02-07 12:35:27 +0300

    UserAccViewModel.kt

---
+++ app/src/main/java/com/cagatayinyurt/ecommercea/viewmodel/UserAccViewModel.kt
@@ -0,0 +1,107 @@
+package com.cagatayinyurt.ecommercea.viewmodel
+
+import android.app.Application
+import android.net.Uri
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.cagatayinyurt.ecommercea.data.User
+import com.cagatayinyurt.ecommercea.util.RegisterValidation
+import com.cagatayinyurt.ecommercea.util.Resource
+import com.cagatayinyurt.ecommercea.util.validateEmail
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.StorageReference
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UserAccViewModel @Inject constructor(
+    private val firestore: FirebaseFirestore,
+    private val auth: FirebaseAuth,
+    private val storage: StorageReference,
+    app: Application
+) : AndroidViewModel(app) {
+
+    private val _user = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val user = _user.asStateFlow()
+
+    private val _updateInfo = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val updateInfo = _updateInfo.asStateFlow()
+
+    init {
+        getUser()
+    }
+
+    fun getUser() {
+        viewModelScope.launch {
+            _user.emit(Resource.Loading())
+        }
+        firestore.collection("user")
+            .document(auth.uid!!)
+            .get()
+            .addOnSuccessListener {
+                val user = it.toObject(User::class.java)
+                user?.let {
+                    viewModelScope.launch {
+                        _user.emit(Resource.Success(it))
+                    }
+                }
+            }.addOnFailureListener {
+                viewModelScope.launch {
+                    _user.emit(Resource.Error(it.message.toString()))
+                }
+            }
+    }
+
+    fun update(user: User, imageUri: Uri?) {
+        val areInputsValid = validateEmail(user.email) is RegisterValidation.Success
+                && user.firstName.trim().isNotEmpty()
+                && user.lastName.trim().isNotEmpty()
+
+        if (!areInputsValid) {
+            viewModelScope.launch {
+                _user.emit(Resource.Error("Check your inputs"))
+            }
+            return
+        }
+
+        viewModelScope.launch {
+            _updateInfo.emit(Resource.Loading())
+        }
+
+        if (imageUri == null) {
+            saveUserInformation(user, true)
+        } else {
+            userInformationWithNewImage(user, imageUri)
+        }
+    }
+
+    private fun userInformationWithNewImage(user: User, imageUri: Uri) {
+
+    }
+
+    private fun saveUserInformation(user: User, shouldRetrievedOldImage: Boolean) {
+        firestore.runTransaction { transaction ->
+            val documentRef = firestore.collection("user")
+                .document(auth.uid!!)
+            if (shouldRetrievedOldImage) {
+                val currentUser = transaction.get(documentRef).toObject(User::class.java)
+                val newUser = user.copy(imagePath = currentUser?.imagePath ?: "")
+                transaction.set(documentRef, newUser)
+            } else {
+                transaction.set(documentRef, user)
+            }
+        }.addOnSuccessListener {
+            viewModelScope.launch {
+                _updateInfo.emit(Resource.Success(user))
+            }
+        }.addOnFailureListener {
+            viewModelScope.launch {
+                _updateInfo.emit(Resource.Error(it.message.toString()))
+            }
+        }
+    }
+}
